fa577599ef3aef87a39a4ab4fc1a386de14bd4dd
Samuel Figueirido <samuelfigueirido@hotmail.com>
2019-04-15 21:57:30 +0200

    feat(development): FormIniciarSesion

--- Proyecto_TPVS.0/Proyecto_TPVS.0/Form1.Designer.cs
+++ Proyecto_TPVS.0/Proyecto_TPVS.0/Form1.Designer.cs
@@ -28,13 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.lblIniciarSesion = new System.Windows.Forms.Label();
+            this.lblRegistrarse = new System.Windows.Forms.Label();
+            this.lblSalir = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(47, 68);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(241, 31);
+            this.txtUsuario.TabIndex = 0;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseña.Location = new System.Drawing.Point(47, 149);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.PasswordChar = '•';
+            this.txtContraseña.Size = new System.Drawing.Size(241, 31);
+            this.txtContraseña.TabIndex = 1;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(43, 41);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(79, 24);
+            this.lblUsuario.TabIndex = 2;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContraseña.Location = new System.Drawing.Point(43, 122);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(111, 24);
+            this.lblContraseña.TabIndex = 3;
+            this.lblContraseña.Text = "Contraseña:";
+            // 
+            // lblIniciarSesion
+            // 
+            this.lblIniciarSesion.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblIniciarSesion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblIniciarSesion.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblIniciarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIniciarSesion.Location = new System.Drawing.Point(47, 212);
+            this.lblIniciarSesion.Name = "lblIniciarSesion";
+            this.lblIniciarSesion.Size = new System.Drawing.Size(241, 46);
+            this.lblIniciarSesion.TabIndex = 3;
+            this.lblIniciarSesion.Text = "Iniciar sesión";
+            this.lblIniciarSesion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRegistrarse
+            // 
+            this.lblRegistrarse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblRegistrarse.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRegistrarse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblRegistrarse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegistrarse.Location = new System.Drawing.Point(289, 53);
+            this.lblRegistrarse.Name = "lblRegistrarse";
+            this.lblRegistrarse.Size = new System.Drawing.Size(139, 46);
+            this.lblRegistrarse.TabIndex = 4;
+            this.lblRegistrarse.Text = "Registrarse";
+            this.lblRegistrarse.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSalir
+            // 
+            this.lblSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblSalir.Image = global::Proyecto_TPVS._0.Properties.Resources.exit;
+            this.lblSalir.Location = new System.Drawing.Point(476, 45);
+            this.lblSalir.Name = "lblSalir";
+            this.lblSalir.Size = new System.Drawing.Size(59, 57);
+            this.lblSalir.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(47, 275);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 22);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Recordar";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Controls.Add(this.txtContraseña);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.txtUsuario);
+            this.groupBox1.Controls.Add(this.lblUsuario);
+            this.groupBox1.Controls.Add(this.lblContraseña);
+            this.groupBox1.Controls.Add(this.lblIniciarSesion);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(116, 134);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(331, 322);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Iniciar sesion";
             // 
             // FormIniciarSesion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1149, 555);
+            this.ClientSize = new System.Drawing.Size(600, 555);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblSalir);
+            this.Controls.Add(this.lblRegistrarse);
             this.Name = "FormIniciarSesion";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TPVS.0";
@@ -43,11 +162,23 @@
             this.Load += new System.EventHandler(this.FormIniciarSesion_Load);
             this.Click += new System.EventHandler(this.FormIniciarSesion_Click);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FormIniciarSesion_KeyUp);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.Label lblIniciarSesion;
+        private System.Windows.Forms.Label lblRegistrarse;
+        private System.Windows.Forms.Label lblSalir;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Proyecto_TPVS.0/Proyecto_TPVS.0/Properties/Resources.Designer.cs
+++ Proyecto_TPVS.0/Proyecto_TPVS.0/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Proyecto_TPVS._0.Properties {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap exit {
+            get {
+                object obj = ResourceManager.GetObject("exit", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
