37a18a1d838343deb263a5ed59f219c1b6610c31
Samuel Figueirido <samuelfigueirido@hotmail.com>
2019-05-20 21:04:53 +0200

    feat(development): new password file to manage the encrypt and decrypt

---
+++ Proyecto_TPVS.0/Proyecto_TPVS.0/Pass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_TPVS._0
+{
+    class Pass
+    {
+        public string encrypt(string password)
+        {
+            string result = string.Empty;
+            byte[] encryted = Encoding.Unicode.GetBytes(password);
+            return Convert.ToBase64String(encryted);
+        }
+
+        public string decrypt(string password)
+        {
+            string result = string.Empty;
+            byte[] decryted = Convert.FromBase64String(password);
+            return Encoding.Unicode.GetString(decryted);
+        }
+    }
+}
