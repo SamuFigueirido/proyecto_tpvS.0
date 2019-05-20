89e117a23a7b1225119a95b4b09e515a3bf5a909
Samuel Figueirido <samuelfigueirido@hotmail.com>
2019-05-20 21:06:02 +0200

    feat(development): new file Factura to manage the orders

---
+++ Proyecto_TPVS.0/Proyecto_TPVS.0/Factura.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_TPVS._0
+{
+    class Factura
+    {
+        private int id;
+        public int Id
+        {
+            set
+            {
+                id = value;
+            }
+            get
+            {
+                return id;
+            }
+        }
+        private string platos;
+        public string Platos
+        {
+            set
+            {
+                platos = value;
+            }
+            get
+            {
+                return platos;
+            }
+        }
+        private double total;
+        public double Total
+        {
+            set
+            {
+                total = value;
+            }
+            get
+            {
+                return total;
+            }
+        }
+
+        public override string ToString()
+        {
+            string[] lines = Platos.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            return "\nFactura nº "+Id+"\nPlatos:\n" + lines.ToString() + "\n\r\nTOTAL: " + total;
+        }
+    }
+}
