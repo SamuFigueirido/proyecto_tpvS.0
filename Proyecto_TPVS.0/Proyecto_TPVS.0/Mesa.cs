d1c43eb872d3b613cd4f156373c5afc5c1122cb9
Samuel Figueirido <samuelfigueirido@hotmail.com>
2019-05-24 00:32:46 +0200

    feat(development): class Mesa created

---
+++ Proyecto_TPVS.0/Proyecto_TPVS.0/Mesa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_TPVS._0
+{
+    class Mesa
+    {
+        private string nombre;
+        public string Nombre
+        {
+            set
+            {
+                nombre = value;
+            }
+            get
+            {
+                return nombre;
+            }
+        }
+        List<string> productos;
+        public List<string> Productos
+        {
+            set
+            {
+                productos = value;
+            }
+            get
+            {
+                return productos;
+            }
+        }
+    }
+}
